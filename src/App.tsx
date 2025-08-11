0ddb9dd3f5e7156f19369e362fea5efc1cee54f2
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-10 21:19:04 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
@@ -9,39 +8,42 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { HorizontalScrollContainer, HorizontalScrollSection } from './components/HorizontalScrollContainer';
 import { FadeInSection } from './components/FadeInSection';
+import { HorizontalScrollProvider } from './contexts/HorizontalScrollContext';
 
 function App(): React.ReactNode {
 
   return (
-    <div className="font-sans text-gray-800">
-      <Header />
-      <main>
-        <Hero />
-        
-        <HorizontalScrollContainer>
-            <HorizontalScrollSection id="about">
-                <FadeInSection variant="horizontal">
-                    <About />
-                </FadeInSection>
-            </HorizontalScrollSection>
-            <HorizontalScrollSection id="services">
-                <FadeInSection variant="horizontal">
-                    <Services />
-                </FadeInSection>
-            </HorizontalScrollSection>
-            <HorizontalScrollSection id="gallery">
-                <FadeInSection variant="horizontal">
-                    <Gallery />
-                </FadeInSection>
-            </HorizontalScrollSection>
-        </HorizontalScrollContainer>
+    <HorizontalScrollProvider>
+      <div className="font-sans text-gray-800">
+        <Header />
+        <main>
+          <Hero />
+          
+          <HorizontalScrollContainer>
+              <HorizontalScrollSection id="about">
+                  <FadeInSection variant="horizontal">
+                      <About />
+                  </FadeInSection>
+              </HorizontalScrollSection>
+              <HorizontalScrollSection id="services">
+                  <FadeInSection variant="horizontal">
+                      <Services />
+                  </FadeInSection>
+              </HorizontalScrollSection>
+              <HorizontalScrollSection id="gallery">
+                  <FadeInSection variant="horizontal">
+                      <Gallery />
+                  </FadeInSection>
+              </HorizontalScrollSection>
+          </HorizontalScrollContainer>
 
-        <FadeInSection>
-          <Contact />
-        </FadeInSection>
-      </main>
-      <Footer />
-    </div>
+          <FadeInSection>
+            <Contact />
+          </FadeInSection>
+        </main>
+        <Footer />
+      </div>
+    </HorizontalScrollProvider>
   );
 }
 
