d5d6d2b82c75d1d4971d5e33de450a2e4d63fc3d
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-10 20:14:54 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -12,15 +14,9 @@ export default {
       },
       colors: {
         'gold': {
-          '100': '#FBF3E6',
-          '200': '#F6E5C9',
-          '300': '#EED6AB',
-          '400': '#E4C186',
-          '500': '#D9AC64',
-          '600': '#C6934A',
-          '700': '#9F763B',
-          '800': '#78592C',
-          '900': '#503C1D',
+          '100': '#FBF3E6', '200': '#F6E5C9', '300': '#EED6AB',
+          '400': '#E4C186', '500': '#D9AC64', '600': '#C6934A',
+          '700': '#9F763B', '800': '#78592C', '900': '#503C1D',
         },
       },
       animation: {
@@ -35,16 +31,32 @@ export default {
           '0%': { opacity: '0', transform: 'translateY(40px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
+        fadeIn: { '0%': { opacity: '0' }, '100%': { opacity: '1' } },
         scaleIn: {
           '0%': { opacity: '0', transform: 'scale(0.95)' },
           '100%': { opacity: '1', transform: 'scale(1)' },
         },
+        'scroll-up': {
+          'from': { transform: 'translateY(0)' },
+          'to': { transform: 'translateY(-50%)' },
+        },
+        'scroll-down': {
+          'from': { transform: 'translateY(-50%)' },
+          'to': { transform: 'translateY(0)' },
+        }
       },
-    }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.text-shadow-subtle': {
+          'text-shadow': '1px 1px 2px rgba(0,0,0,0.1)',
+        },
+        '.text-shadow-strong': {
+          'text-shadow': '2px 2px 8px rgba(0,0,0,0.5)',
+        },
+      })
+    })
+  ],
 }
