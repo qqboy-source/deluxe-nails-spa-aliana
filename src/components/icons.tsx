18a8a36a8076b15da5737a9b1af396b1d88971c8
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-12 18:41:13 -0400

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -38,3 +38,15 @@ export const ClockIcon: React.FC<IconProps> = ({ className }) => (
         <polyline points="12 6 12 12 16 14"></polyline>
     </svg>
 );
+
+export const LeftArrowIcon: React.FC<IconProps> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+      <path d="M15 18l-6-6 6-6"/>
+    </svg>
+);
+
+export const RightArrowIcon: React.FC<IconProps> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <path d="M9 18l6-6-6-6"/>
+    </svg>
+);
