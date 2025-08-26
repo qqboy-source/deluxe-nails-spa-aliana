1685c7374cf3e6838f752f8403016cceaf3d97c1
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-26 19:29:27 -0400

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -50,3 +50,15 @@ export const RightArrowIcon: React.FC<IconProps> = ({ className }) => (
         <path d="M9 18l6-6-6-6"/>
     </svg>
 );
+
+export const ArrowDownIcon: React.FC<IconProps> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <path d="m6 9 6 6 6-6"/>
+    </svg>
+);
+
+export const ArrowUpIcon: React.FC<IconProps> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <path d="m18 15-6-6-6 6"/>
+    </svg>
+);
