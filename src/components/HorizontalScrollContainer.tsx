d4f783b82406149d3ba1988d7a947fdfbc056616
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-27 21:07:57 -0400

    Update HorizontalScrollContainer.tsx

--- src/components/HorizontalScrollContainer.tsx
+++ src/components/HorizontalScrollContainer.tsx
@@ -1,6 +1,6 @@
 
 import React, { useRef, useLayoutEffect, Children, useState, cloneElement, isValidElement, ReactNode, useCallback } from 'react';
-import { useHorizontalScroll } from '../HorizontalScrollContext';
+import { useHorizontalScroll } from '../App';
 
 interface HorizontalScrollSectionProps {
     children: React.ReactNode;
