ff71db7255267cc85375d4394add994d1437fe80
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-27 21:07:36 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import { useHorizontalScroll } from '../HorizontalScrollContext';
+import { useHorizontalScroll } from '../App';
 
 export const Header: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
