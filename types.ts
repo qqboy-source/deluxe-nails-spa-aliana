c2c0a9665bbe164636889d55860afbff58dfa3e5
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-08-29 14:21:45 -0400

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+
+export interface ServiceItem {
+    name: string;
+    price: string;
+    description?: string;
+}
+
+export interface ServiceCategory {
+    category: string;
+    note?: string;
+    layout?: 'grid' | 'list';
+    items: ServiceItem[];
+}
